1667e06707d177a75cb7d11cf130530c05b6447f
Mutuba <danielmutubait@gmail.com>
2022-09-19 00:10:57 +0300

    fix typo

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def create
     @current_user = current_user
     @message = @current_user.messages.create(content: msg_params[:content], room_id: params[:room_id])
+    Turbo::StreamsChannel.broadcast_append_to(current_user)
     respond_to do |format|
       format.turbo_stream { redirect_to room_messages_path }
     end
